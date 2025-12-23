3405534193d59e29aa3e633c2068aed34ece35de
Devansh Hapalia <devansh.hapalia@marutitech.com>
2025-12-23 18:00:06 +0530

    feat:set up type script

---
+++ TypeScript/first-typescript-project/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript 🚀`;
+}
+console.log(greet("Devansh"));
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/first-typescript-project/src/primitiveType.ts
@@ -0,0 +1,33 @@
+// Boolean type examples
+let isActive: boolean = true;
+let hasPermission = false; // TypeScript infers 'boolean' type
+
+// Output the values
+console.log(isActive);
+console.log(hasPermission);
+
+// Number type examples
+let decimal: number = 6;
+let hex: number = 0xf00d;      // Hexadecimal
+let binary: number = 0b1010;   // Binary
+let octal: number = 0o744;     // Octal
+let float: number = 3.14;      // Floating point
+
+// Output the values
+console.log(decimal);
+console.log(hex);
+console.log(binary);
+console.log(octal);
+console.log(float);
+
+// String type examples
+let color: string = "blue";
+let fullName: string = 'John Doe';
+let age: number = 30;
+let sentence: string = `Hello, my name is ${fullName} and I'll be ${age + 1} next year.`;
+
+// Output the values
+console.log(color);
+console.log(fullName);
+console.log(age);
+console.log(sentence);
