20226a37dc86834514ac3cf12abac5d653257868
Devansh Hapalia <devansh.hapalia@marutitech.com>
2026-01-05 11:16:08 +0530

    feat:adding react work

---
+++ React/Hooks/src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+import Navbar from "./Navbar";
+
+function App() {
+  const [user, setUser] = useState("John Doe");
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Navbar />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
